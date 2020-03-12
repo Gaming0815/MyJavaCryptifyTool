b2bd6f6c5e1c6148c8af5026a9858d7e3b3d4283
Gaming0815 <i.niklas.fischer@iCloud.com>
2020-03-12 17:20:03 +0100

    Created main and Schriffierer Class, written as far as I remembered.

---
+++ src/SchriffierenTool.java
@@ -0,0 +1,8 @@
+
+public class SchriffierenTool {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/Schriffierer.java
@@ -0,0 +1,22 @@
+
+public class Schriffierer {
+
+	public void schriffierenCA(String text, int schlüssel) {
+		
+		String erg = "";
+		
+		for (int i = 0; i < text.length(); i = i + 1) {
+			
+			int x = (int) text.charAt(i);
+			
+			
+		}
+		
+	}
+	
+	public void deschriffierenCA(String text, int schlüssel) {
+		
+		
+	}
+	
+}
